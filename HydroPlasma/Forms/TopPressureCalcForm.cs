f8c09ce353fd2595197f347aac0511bc1c336877
jiangchang <jiangchang@yiche.com>
2018-01-19 16:56:43 +0800

    v7

--- HydroPlasma/Forms/TopPressureCalcForm.cs
+++ HydroPlasma/Forms/TopPressureCalcForm.cs
@@ -36,6 +36,7 @@ namespace HydroPlasma.Forms
                     CalcPressureByEnergyX(model);
                     break;
                 case "电极间距":
+                    CalcPressureByProbeLenX(model);
                     break;
             }
             //double  minEnergy = 100 ;
@@ -56,6 +57,66 @@ namespace HydroPlasma.Forms
             //chartMaxPre.Series[0].Points.DataBindXY(energy, maxPre);
         }
 
+        private void CalcPressureByProbeLenX(MaxPressureModel model)
+        {
+            //throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int ponitNum = 20;
+            //每一条线
+            this.chartMaxPre.Series.Clear();
+            double maxX = 0;
+            for (int i = 0; i < model.LineNum; i++)
+            {
+                //当前线下的放电能量
+                //double currentProbeLength = model.ProbeMinLength + i * (model.ProbeMaxLength - model.ProbeMinLength) / (model.LineNum - 1);
+               
+                //最高最低能量
+                double minEnergy = 0.5 * model.CapacityVal * model.OutputVoltage * model.OutputVoltage * model.MinCapacityNum;
+                double maxEnergy = 0.5 * model.CapacityVal * model.OutputVoltage * model.OutputVoltage * model.MaxCapacityNum;
+                //当前线下的能量
+                double currentEnergy = minEnergy + i* (maxEnergy - minEnergy) / (model.LineNum - 1);
+                //x坐标
+                double[] proLenArr = new double[ponitNum];
+                for (int j = 0; j < proLenArr.Length; j++)
+                {
+                    proLenArr[j] = model.ProbeMinLength + j * (model.ProbeMaxLength - model.ProbeMinLength) / (ponitNum - 1);
+                }
+                //计算最大电极
+                maxX = proLenArr[ponitNum-1] > maxX ? proLenArr[ponitNum-1] : maxX;
+                double[] maxPressureArr = new double[ponitNum];
+                //计算y坐标
+                for (int k = 0; k < proLenArr.Length; k++)
+                {
+                    maxPressureArr[k] = model.ComplexFuncBeita * 100 * Math.Sqrt(currentEnergy * 0.24 / (proLenArr[k] / 10)) / 4;
+                }
+                //画图
+                Series ss = new Series("放电能量" + (int)currentEnergy + "J");
+                ss.Points.DataBindXY(proLenArr, maxPressureArr);
+                ss.BorderWidth = 3;
+                ss.ChartType = SeriesChartType.Spline;
+                this.chartMaxPre.Series.Add(ss);
+            }
+            //修改X坐标轴
+            int maxX2 = ((int)maxX / 10 + 1) * 10;
+            this.chartMaxPre.ChartAreas[0].AxisX.Minimum = 0;
+            this.chartMaxPre.ChartAreas[0].AxisX.Maximum = maxX2;
+            //修改坐标轴标题
+            this.chartMaxPre.ChartAreas[0].AxisX.Title = "电极间距 mm";
+            this.chartMaxPre.ChartAreas[0].AxisY.Title = "峰值压力 MPa";
+            //不显示网格线
+            this.chartMaxPre.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Transparent;
+            this.chartMaxPre.ChartAreas[0].AxisX.ArrowStyle = AxisArrowStyle.Triangle;
+            this.chartMaxPre.ChartAreas[0].AxisY.ArrowStyle = AxisArrowStyle.Triangle;
+            // this.chartMaxPre.ChartAreas[0].AxisX.IsMarginVisible = false;
+            //坐标轴刻度类型
+            TickMark t = new TickMark();
+            t.TickMarkStyle = TickMarkStyle.None;
+            this.chartMaxPre.ChartAreas[0].AxisX.MajorTickMark = t;
+            this.chartMaxPre.ChartAreas[0].AxisY.MajorTickMark = t;
+            //this.chartMaxPre.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Transparent;
+           
+        }
+
         /// <summary>
         ///将电容器能量作为X坐标
         /// </summary>
@@ -67,8 +128,6 @@ namespace HydroPlasma.Forms
             //每一条线
             this.chartMaxPre.Series.Clear();
             double maxX = 0;
-
-
             for (int i = 0; i < model.LineNum; i++)
             {
                 //当前线下的电极距离
@@ -91,7 +150,7 @@ namespace HydroPlasma.Forms
                     maxPressureArr[k] = model.ComplexFuncBeita *100* Math.Sqrt(energyArr[k] * 0.24 / (currentProbeLength / 10)) / 4;
                 }
                 //画图
-                Series ss = new Series( "电极距离"+currentProbeLength+"mm");
+                Series ss = new Series( "电极距离"+(int)currentProbeLength+"mm");
                 ss.Points.DataBindXY(energyArr, maxPressureArr);
                 ss.BorderWidth = 3;
                 ss.ChartType = SeriesChartType.Spline;
@@ -109,12 +168,11 @@ namespace HydroPlasma.Forms
             this.chartMaxPre.ChartAreas[0].AxisX.ArrowStyle = AxisArrowStyle.Triangle;
             this.chartMaxPre.ChartAreas[0].AxisY.ArrowStyle = AxisArrowStyle.Triangle;
             // this.chartMaxPre.ChartAreas[0].AxisX.IsMarginVisible = false;
+            //坐标轴刻度类型
             TickMark t = new TickMark();
             t.TickMarkStyle = TickMarkStyle.None;
             this.chartMaxPre.ChartAreas[0].AxisX.MajorTickMark = t;
             this.chartMaxPre.ChartAreas[0].AxisY.MajorTickMark = t;
-
-
             //this.chartMaxPre.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Transparent;
         }
 
